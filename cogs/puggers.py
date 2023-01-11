eada299b5337992979f22c1163d041e697f574a1
hestonhoffman <hestonhoffman@gmail.com>
2023-01-11 12:24:15 -0800

    Add pugbomb

---
+++ cogs/puggers.py
@@ -0,0 +1,38 @@
+from discord.ext import commands
+import json
+from dotenv import load_dotenv
+from urllib import parse, request
+import os
+import random
+
+class Puggers(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None      
+
+    @commands.command()
+    async def pugbomb(self, ctx):
+        # Print a random pug
+        params = parse.urlencode({
+            "q": "pug",
+            "api_key": f"{os.getenv('GIPHY_TOKEN')}",
+            "limit": "25"
+        })
+        url = "http://api.giphy.com/v1/gifs/search"
+        with request.urlopen("".join((url, "?", params))) as response:
+            data = json.loads(response.read())
+        
+        urls = []
+        for key, value in data.items():
+            if key == "data":
+                for dict in value:
+                    urls.append(dict["url"])
+        
+        select_5 = random.sample(urls, 5)
+        for entry in urls:
+            await ctx.send(entry)
+
+        
+
+
+        #await ctx.send("https://media.giphy.com/media/1MCY9kPHuGG1G/giphy.gif")
